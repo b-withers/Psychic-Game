a7e0810844a47efdda020410fc4a278bcd873e96
b-withers <brady.withers@gmail.com>
2018-08-08 13:46:21 -0600

    script.js notes started

--- assets/javaScript/script.js
+++ assets/javaScript/script.js
@@ -1,42 +1,60 @@
+//sounds for the game
 var win = new Audio('assets/sounds/no.mp3');
 var lose = new Audio('assets/sounds/laugh.wav');
-// start of logic for game
+var push = new Audio('assets/sounds/push.mp3');
+//variables used within the game
+//empty variable for completter
 var completter = "";
-var alphabet = "abcdefghijklmnopqrstuvwxyz";
+var alphabet = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z"];
 var guessesleft = 10;
 var guessedletters = [];
 var wins = 1;
 var losses = 1;
+//creates buttons on screen
+createLetterButtons();
 
-//computer picking the number:
+// start of logic for game
+//computer picking the random letter from alphabet array
 completter = alphabet[Math.floor(Math.random() * alphabet.length)];
 //logs computer picked letter
 console.log(completter);
-
+//keyboard event registering letter pressed on keyboard
 document.onkeyup = function(event) {
+//console log the key pressed
 console.log(event);
 //display guess on screen, add to existing guesses
 guessedletters.push(event.key);
+//adds guessed letter to screen
 document.getElementById("lettersGuessed").innerHTML = guessedletters;
 console.log(guessedletters);
-
-//check to see if same as completter
+//compare event.key with completter, if it is the same as the completter the following happens
 if (event.key===completter){
+	//simply says win in console
 	console.log("win");
+	//if user guesses letter correctly, 1 point (wins ++) is added to the wins div
 	document.getElementById("wins").innerHTML = wins ++;
+	//win sound is played from variable above
 	win.play();
+	//game is reset by the resetgame function (below)
 	resetgame();
 }
 
-//subtract one from guesses left
+//if the "if" statement is not ture, we run the following "else"
 else {
+	//subtract 1 from the guessesleft variable set to 10 guesses above
 	guessesleft --;
+	//we get the "guesses-left" ID on our HTML and apply the above
 		document.getElementById("guesses-left").innerHTML = guessesleft;
-//when guess left gets to zero, games ends		
+//when guess left gets to zero, games ends
+//the following is checked, is guessesleft = 0?	if so, run the following	
 if (guessesleft === 0){
-        console.log("loss");
-        document.getElementById("losses").innerHTML = losses ++;
-        lose.play();
+	//console.log "loss"
+		console.log("loss");
+		//add 1 to the "losses" ID on our HTML
+		document.getElementById("losses").innerHTML = losses ++;
+		//play the lose sound var from above
+		lose.play();
+		//game is reset by the resetgame function (below)
          	resetgame();
          }
 }
@@ -52,4 +70,28 @@ function resetgame(){
 	document.getElementById("lettersGuessed").innerHTML = guessedletters;
 	document.getElementById("guesses-left").innerHTML =  guessesleft;
 }
+
+
+$(document).ready(function() {
+//Add click event to the letter buttons
+$(".letter-button").on("click", function(){
+  push.play();
+  console.log(this);       
+})
+});
+
+
+
+//creates the buttons on the screen
+function createLetterButtons() {
+    for (var i=0; i<alphabet.length; i++) {
+      var letterBtn = $("<button>");
+      letterBtn.addClass("letter-button letter letter-button-color");
+      letterBtn.attr("data-letter", alphabet[i]);
+      letterBtn.text(alphabet[i]);
+      $("#buttons").append(letterBtn);
+    }
+};
+
+
 
